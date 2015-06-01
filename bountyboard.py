cfa90427b3fd03e51b132fb7d15ca5cde6bcb84f
Jesse Vera <jesse.vera@bazaarvoice.com>
2015-06-01 14:12:19 -0500

    Small changes to accordian elements, trying to get CSS magic working

--- bountyboard.py
+++ bountyboard.py
@@ -1,8 +1,8 @@
 #!/bin/env python
-from config import SERVER_NAME
-from config import SERVER_PORT
+# from config import SERVER_NAME
+# from config import SERVER_PORT
 from app import app
 
 if __name__ == '__main__':
     # app.run(SERVER_NAME, int(SERVER_PORT), debug=True)
-    app.run(debug=True)
+	app.run(debug=True)
