adf6808267a7b3c03245e156d2c6489042762eb4
Jesse Vera <jesse.vera@bazaarvoice.com>
2015-06-03 14:57:41 -0500

    Reworked forms, renamed SERVER_NAME to SERVER_HOST_NAME

--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from flask.ext.bootstrap import Bootstrap
 
 from app import app, db, models
-from .forms import BountyForm
+from .forms import NewBountyForm, SortBountyForm
 bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    bounty_form = BountyForm()
-    # if bounty_form.validate_on_submit():
-    # 	return "YAY"
     bounties = models.Bounty.query.all()
     projects = models.Project.query.all()
-    bounty_form.project.choices = [(p.id, p.name) for p in projects]
-    return render_template('index.html', bounties=bounties, projects=projects, bounty_form=bounty_form)
+
+    new_bounty_form = NewBountyForm(projects)
+    sort_bounty_form = SortBountyForm(projects)
+    if new_bounty_form.validate_on_submit():
+    	return "YAY"
+
+    return render_template('index.html', bounties=bounties, projects=projects, new_bounty_form=new_bounty_form, sort_bounty_form=sort_bounty_form)
+
+
+    
