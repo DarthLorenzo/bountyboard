452f456956266cf048ac693688b0b8dc89899aca
Jesse Vera <jesse.vera@bazaarvoice.com>
2015-06-04 11:00:10 -0500

    Separated bounties/projects, added tag labels

--- app/forms.py
+++ app/forms.py
@@ -1,30 +1,25 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, TextAreaField, SelectField, SelectMultipleField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SelectField, SelectMultipleField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class NewBountyForm(Form):
 
-	def __init__(self, projects):
-
-		project_options = [(p.id, p.name) for p in projects]
-
-		title = StringField('Overview', validators=[DataRequired()])
-		description = TextAreaField('Description', validators=[DataRequired()])
-		project = SelectField('Project', choices=project_options, validators=[DataRequired()])
-		
-		Form.__init__(self)
-
+    title = StringField('Overview', validators=[DataRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    project = SelectField('Project', coerce=int)
+    submit = SubmitField("Save Bounty")
 
 
 class SortBountyForm(Form):
 
-	def __init__(self, projects):
+    sort_options = [(1, "Newest"), (2, "Oldest"), (3, "Biggest Booty")]
 
-		project_options = [(p.id, p.name) for p in projects]
-		sort_options = [(1, "Newest"), (2, "Oldest"), (3, "Biggest Booty")]
+    sortBy = SelectField('Sort By', choices=sort_options)
+    projectFilter = SelectMultipleField('Project')
+    tagFilter = SelectMultipleField("Languages", choices=sort_options)
 
-		sortBy = SelectField('Sort By', choices=sort_options)
-		projectFilter = SelectMultipleField('Project', choices=project_options)
-		tagFilter = SelectMultipleField("Languages", choices=sort_options)
-		
-		Form.__init__(self)
+
+class NewTagForm(Form):
+
+    tag = StringField('New tag', validators=[DataRequired(), Length(2,15)])
+    submit = SubmitField("Add Tag")
--- app/views.py
+++ app/views.py
@@ -1,21 +1,45 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 
 from app import app, db, models
-from .forms import NewBountyForm, SortBountyForm
+from .forms import NewBountyForm, SortBountyForm, NewTagForm
 bootstrap = Bootstrap(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    return redirect(url_for('bounties'))
+
+@app.route('/bounties', methods=['GET', 'POST'])
+def bounties():
     bounties = models.Bounty.query.all()
     projects = models.Project.query.all()
 
-    new_bounty_form = NewBountyForm(projects)
-    sort_bounty_form = SortBountyForm(projects)
+    new_bounty_form = NewBountyForm()
+    sort_bounty_form = SortBountyForm()
+
+    new_bounty_form.project.choices = [(p.id, p.name) for p in projects]
+    sort_bounty_form.projectFilter.choices = [(p.id, p.name) for p in projects]
+
     if new_bounty_form.validate_on_submit():
-    	return "YAY"
+        new_bounty = models.Bounty(project_id=new_bounty_form.data['project'],
+                                   title=new_bounty_form.data['title'],
+                                   description=new_bounty_form.data['description'],
+                                   active=True)
+        db.session.add(new_bounty)
+        db.session.commit()
+        return redirect(url_for('bounties'))
 
-    return render_template('index.html', bounties=bounties, projects=projects, new_bounty_form=new_bounty_form, sort_bounty_form=sort_bounty_form)
+    return render_template('bounties.html', bounties=bounties, projects=projects, new_bounty_form=new_bounty_form, sort_bounty_form=sort_bounty_form)
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    projects = models.Project.query.all()
+    new_tag_form = NewTagForm()
+
+    if new_tag_form.validate_on_submit():
+        return "YAY"
+
+    return render_template('projects.html', projects=projects, new_tag_form=new_tag_form)
 
 
-    
+
